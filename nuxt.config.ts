f2fe7f38ec7085075bcfb1d6880e56bae07bb5b9
Cody Bontecou <bontecouc@gmail.com>
2022-05-05 14:38:40 -1000

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  css: ['vuetify/lib/styles/main.sass'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
+})
---
+++ plugins/vuetify.js
@@ -0,0 +1,12 @@
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export default defineNuxtPlugin(nuxtApp => {
+  const vuetify = createVuetify({
+    components,
+    directives,
+  })
+
+  nuxtApp.vueApp.use(vuetify)
+})
