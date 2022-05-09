b44c0150b5e154ed50362e83931e0ebe4895692f
Cody Bontecou <bontecouc@gmail.com>
2022-05-09 13:21:10 -1000

    Added mdi dependency and config to allow v-icon to work

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  css: ['vuetify/lib/styles/main.sass'],
+  css: ['vuetify/lib/styles/main.sass', 'mdi/css/materialdesignicons.min.css'],
   build: {
     transpile: ['vuetify'],
   },
