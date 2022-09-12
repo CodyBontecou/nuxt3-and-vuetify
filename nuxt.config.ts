26f149bae1439cf3b99665cbebc529de6536e568
Wm. C <36806141+willcdotdev@users.noreply.github.com>
2022-09-12 03:29:41 -0400

    Update path to match package.json
    See comment for package.json regarding mdi package now being @mdi/font since v2.2.43

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  css: ['vuetify/lib/styles/main.sass', 'mdi/css/materialdesignicons.min.css'],
+  css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
   build: {
     transpile: ['vuetify'],
   },
